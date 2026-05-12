119c0761c7f65bc2202f1aadd8bcef113cd54508
MattDaddy7 <dkbeatty@verizon.net>
2026-05-12 09:59:29 -0400

    changes

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  include Authentication
+  # Allow all users to view inventory
+  allow_unauthenticated_access only: %i[ index show ]
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+end
